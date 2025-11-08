db4c6e79d84ebfd6f3869b28aea5f6f2dcae8eec
Shahe Alam <alamshahe9044@gmail.com>
2025-11-08 19:05:37 +0530

    🚀 Changes in python.py File ✅

--- Git and GitHub/python.py
+++ Git and GitHub/python.py
@@ -1,2 +1,11 @@
 print("Hello World!, this is a crash course in Python.")
 
+# Variables 
+
+name = "Alice"
+age = 30
+height = 5.5
+is_student = False
+print(f"Name: {name}, Age: {age}, Height: {height}, Is Student: {is_student}")
+
+
